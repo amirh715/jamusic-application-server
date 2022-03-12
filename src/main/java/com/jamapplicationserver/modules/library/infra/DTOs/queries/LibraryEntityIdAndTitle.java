d2d0ec2875fa001f2d03ec879fddc3b0d3b01476
Amirhossein Al. <amirh715@gmail.com>
2022-03-12 23:43:50 +0330

    Add published field so that access control can be enforced

--- src/main/java/com/jamapplicationserver/modules/library/infra/DTOs/queries/LibraryEntityIdAndTitle.java
+++ src/main/java/com/jamapplicationserver/modules/library/infra/DTOs/queries/LibraryEntityIdAndTitle.java
@@ -17,21 +17,25 @@ public class LibraryEntityIdAndTitle implements IQueryResponseDTO {
     
     public final String id;
     public final String title;
+    public final Boolean published;
     
-    protected LibraryEntityIdAndTitle(String id, String title) {
+    protected LibraryEntityIdAndTitle(String id, String title, Boolean published) {
         this.id = id;
         this.title = title;
+        this.published = published;
     }
     
     public static final LibraryEntityIdAndTitle create(LibraryEntityModel model) {
         return new LibraryEntityIdAndTitle(
                 model.getId().toString(),
-                model.getTitle()
+                model.getTitle(),
+                model.isPublished()
         );
     }
     
     @Override
     public LibraryEntityIdAndTitle filter(UserRole role) {
+        if(!this.published && role.isSubscriber()) return null;
         return this;
     }
     
